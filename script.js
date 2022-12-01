3982eb030ceed755a2bb65d669e302b703da28e9
falah <falah@duck.com>
2022-12-01 21:27:31 +0530

    Added Major Changes

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let hamburgerMenu = document.getElementById("hamburger");
 let navBar = document.querySelector("nav ul");
+let backdropElement = document.getElementById('backdrop');
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('is-active');
     navBar.classList.toggle('is-active');
+    backdropElement.classList.toggle('is-active');
+})
+
+
+let navLinks = document.querySelectorAll('.links');
+navLinks.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburgerMenu.classList.remove('is-active');
+        navBar.classList.remove('is-active');
+    })
 })
